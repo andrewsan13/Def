f6977ff4c8ea03f2c54d96823e949e9cc85defc2
andrewsan13 <51412585+andrewsan13@users.noreply.github.com>
2023-03-11 15:58:32 +0200

    update cpp files

---
+++ Source/Def/Public/Items/MyItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "MyItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEF_API AMyItem : public AItem
+{
+	GENERATED_BODY()
+	
+};
